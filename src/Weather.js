2e938710f6cd001780db2e5938a55d3c25b000e2
Michael Simms <81196688+gingernapoleon@users.noreply.github.com>
2021-07-03 16:42:13 -0700

    added span for temperature and unit

--- src/Weather.js
+++ src/Weather.js
@@ -26,7 +26,8 @@ export default function Weather() {
     src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
     alt="Mostly Cloudy"
     />
-    6°C
+    <span className="temperature">6</span>
+    <span className="unit">°C</span>
     </div>
         <div className="col-6">
 <ul>
