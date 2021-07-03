96ea07ea6afc7c6818824a29df88f00033df5dea
Michael Simms <81196688+gingernapoleon@users.noreply.github.com>
2021-07-03 15:35:59 -0700

    redo, for weather-app-react

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Weather from "./Weather.js";
+import './App.css';
+
+export default function App() {
+  return (
+  <div className="App">
+  <div className="container">
+    <Weather />
+    <footer>
+      This project was coded by{" "}
+      <a href="https://www.linkedin.com/in/napoleon-6b89b756/" 
+      target="_blank" 
+      rel="noreferrer">
+      Ginger Napoleon
+      </a>{" "} 
+      and is{" "}  
+      <a href="https://github.com/gingernapoleon/weather-app-react" 
+      target="_blank" 
+      rel="noreferrer"> 
+      open-sourced on Github
+      </a> 
+    </footer>
+    </div>
+    </div>
+  );
+}
