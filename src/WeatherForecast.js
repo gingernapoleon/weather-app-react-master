43473db07b8c0929c06949eeed617de3eb4a766a
Michael Simms <81196688+gingernapoleon@users.noreply.github.com>
2021-07-04 00:13:49 -0700

    updated styling and key

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,7 +17,7 @@ export default function WeatherForecast(props) {
   }
 
   function load() {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiKey = "da16df2d631e0a7dbbac5c7b96916177";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
